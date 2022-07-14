7aea851c25ec54c0294e0a6e6f83ca0deea722cb
Sakshi Shah <sakshi.shah299@gmail.com>
2022-07-14 06:15:31 +0530

    Project Finished!

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -13,7 +13,10 @@ return  async (dispatch) =>{
     };
     try{
        const cartData=await fetchData(); 
-       dispatch(cartActions.replaceCart(cartData));
+       dispatch(cartActions.replaceCart({
+        items:cartData.items || [],
+        totalQuantity:cartData.totalQuantity
+      }));
     }catch(error){
         dispatch(
             uiActions.showNotification({
@@ -42,7 +45,7 @@ export const sendCartData = (cart) => {
           'https://react-http-d9651-default-rtdb.firebaseio.com/cart.json',
           {
             method: 'PUT',
-            body: JSON.stringify(cart),
+            body: JSON.stringify({items:cart.items, totalQuantity:cart.totalQuantity}),
           }
         );
   
